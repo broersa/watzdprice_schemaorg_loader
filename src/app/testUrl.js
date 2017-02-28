bbb02275fd52590bc9064445ec2dac43e1a32ce3
Andre Broers <broersa@Andres-MacBook-Air.local>
2017-02-28 14:54:42 +0100

    dryrun added

--- src/app/testUrl.js
+++ src/app/testUrl.js
@@ -1,7 +1,7 @@
 var schema = require("./schemaParser.js");
 var request = require("request");
 
-processSite('https://www.travelbags.nl/bear-design-dark-nature-heuptas-brown.html');
+processSite(process.argv[2]);
 
 function processSite(item) {
   setTimeout(function (url) {
@@ -21,38 +21,38 @@ function processSite(item) {
           for (var i = 0, ilen = msg.elems.length; i < ilen; i++) {
             if (msg.elems[i].product) {
               for (var j = 0, jlen = msg.elems[i].product.length; j < jlen; j++) {
-                if (msg.elems[i].product[j].name && msg.elems[i].product[j].name.text) {
-                  product.name = msg.elems[i].product[j].name.text;
+                if (!product.name && msg.elems[i].product[j].name && msg.elems[i].product[j].name.text) {
+                  product.name = msg.elems[i].product[j].name.text.substring(0,255);
                 }
-                if (msg.elems[i].product[j].gtin13 && msg.elems[i].product[j].gtin13.text) {
-                  product.eancode = msg.elems[i].product[j].gtin13.text;
+                if (!product.eancode && msg.elems[i].product[j].gtin13 && msg.elems[i].product[j].gtin13.text) {
+                  product.eancode = msg.elems[i].product[j].gtin13.text.substring(0,255);
                 }
-                if (msg.elems[i].product[j].gtin13 && msg.elems[i].product[j].gtin13.content) {
-                  product.eancode = msg.elems[i].product[j].gtin13.content;
+                if (!product.eancode && msg.elems[i].product[j].gtin13 && msg.elems[i].product[j].gtin13.content) {
+                  product.eancode = msg.elems[i].product[j].gtin13.content.substring(0,255);
                 }
-                if (msg.elems[i].product[j].brand && msg.elems[i].product[j].brand.text) {
-                  product.brand = msg.elems[i].product[j].brand.text;
+                if (!product.brand && msg.elems[i].product[j].brand && msg.elems[i].product[j].brand.text) {
+                  product.brand = msg.elems[i].product[j].brand.text.substring(0,255);
                 }
-                if (msg.elems[i].product[j].brand && msg.elems[i].product[j].brand.content) {
-                  product.brand = msg.elems[i].product[j].brand.content;
+                if (!product.brand && msg.elems[i].product[j].brand && msg.elems[i].product[j].brand.content) {
+                  product.brand = msg.elems[i].product[j].brand.content.substring(0,255);
                 }
-                if (msg.elems[i].product[j].price && msg.elems[i].product[j].price.text) {
+                if (!product.price && msg.elems[i].product[j].price && msg.elems[i].product[j].price.text) {
                   product.price = parseFloat(msg.elems[i].product[j].price.text.replace(',','.'));
                 }
-                if (msg.elems[i].product[j].price && msg.elems[i].product[j].price.content) {
+                if (!product.price && msg.elems[i].product[j].price && msg.elems[i].product[j].price.content) {
                   product.price = parseFloat(msg.elems[i].product[j].price.content.replace(',','.'));
                 }
-                if (msg.elems[i].product[j].image && msg.elems[i].product[j].image.content) {
-                  product.image = msg.elems[i].product[j].image.content;
+                if (!product.image && msg.elems[i].product[j].image && msg.elems[i].product[j].image.content) {
+                  product.image = msg.elems[i].product[j].image.content.substring(0,1999);
                 }
-                if (msg.elems[i].product[j].image && msg.elems[i].product[j].image.src) {
-                  product.image = msg.elems[i].product[j].image.src;
+                if (!product.image && msg.elems[i].product[j].image && msg.elems[i].product[j].image.src) {
+                  product.image = msg.elems[i].product[j].image.src.substring(0,1999);
                 }
-                if (msg.elems[i].product[j].description && msg.elems[i].product[j].description.text) {
-                  product.description = msg.elems[i].product[j].description.text;
+                if (!product.description && msg.elems[i].product[j].description && msg.elems[i].product[j].description.text) {
+                  product.description = msg.elems[i].product[j].description.text.substring(0,1999);
                 }
-                if (msg.elems[i].product[j].description && msg.elems[i].product[j].description.content) {
-                  product.description = msg.elems[i].product[j].description.content;
+                if (!product.description && msg.elems[i].product[j].description && msg.elems[i].product[j].description.content) {
+                  product.description = msg.elems[i].product[j].description.content.substring(0,1999);
                 }
               }
             }
